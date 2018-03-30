1a31473ded6003230ab396191a2d772f0534e5ec
TreeAntSan <treeantsan@gmail.com>
2018-03-29 18:07:09 -0700

    No real change. Just spent the last 89 minutes figuring out how to debug the server.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import deline from 'deline';
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.header("Content-Type", "text/html").send("<html><body><h1>404</h1></body></html>");
+  const timestamp = new Date().toLocaleString();
+  res.header("Content-Type", "text/html").send(deline`<html><body>
+    <h1>404</h1>
+    <p>Server time is ${timestamp}</p>
+    </body></html>`);
 });
 
 module.exports = router;
