c87e80d4c9d4235cc3850d44bb63ca2e1341c7dc
TreeAntSan <treeantsan@gmail.com>
2018-04-12 18:35:32 -0700

    Client: Made deleteMovie work with the Apollo Client cache. I'm honestly surprised I pulled this off without needing to tear my hair out.

--- client/src/components/movies/Movies.js
+++ client/src/components/movies/Movies.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { graphql, compose } from "react-apollo";
+import { graphql, compose, withApollo } from "react-apollo";
 import gql from "graphql-tag";
 import { withRouter } from "react-router";
 import { Container } from "semantic-ui-react";
@@ -10,14 +10,26 @@ import LoadingError from "../LoadingError";
 
 // TODO Bug: page doesn't refresh when navigated to it when there is a new movie saved
 class Movies extends Component {
-   handleDeleteClick = (e, movieId) => {
+  handleDeleteClick = (e, movieId) => {
     e.preventDefault();
-    this.props.deleteMovie({
+    this._executeDelete(movieId);
+  };
+
+  _executeDelete = (movieId) => {
+    // Inspiration taken from:
+    // https://github.com/apollographql/apollo-client/blob/master/docs/source/basics/mutations.md#updating-the-cache-after-a-mutation
+    this.props.client.mutate({
+      mutation: DELETE_MOVIE_MUTATION,
       variables: {
         id: movieId,
       },
-    });//.then(this.props.allMovies.refetch());
-    // TODO need to make it so that deleting actually DOES SOMETHING
+      update: (proxy, { data: { deleteMovie }}) => {
+        const data = proxy.readQuery({ query: ALL_MOVIES_QUERY });
+        // Remove the movie using the mutation return data.
+        data.movies.splice(data.movies.findIndex(movie => movie.id === deleteMovie.id), 1);
+        proxy.writeQuery({ query: ALL_MOVIES_QUERY, data });
+      },
+    });
   };
 
   render() {
@@ -41,7 +53,6 @@ class Movies extends Component {
 
 Movies.propTypes = {
   allMovies: PropTypes.object.isRequired,
-  deleteMovie: PropTypes.func.isRequired,
 };
 
 const ALL_MOVIES_QUERY = gql`
@@ -82,6 +93,6 @@ const DELETE_MOVIE_MUTATION = gql`
 `;
 
 export default withRouter(compose(
+  withApollo,
   graphql(ALL_MOVIES_QUERY, { name: "allMovies" }),
-  graphql(DELETE_MOVIE_MUTATION, { name: "deleteMovie" }),
 )(Movies));
